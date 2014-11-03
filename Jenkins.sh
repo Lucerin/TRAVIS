2bc57bc28cdfb94df254820862421bfcbfe83fa6
Lucerin <lecc.14@hotmail.com>
2014-11-03 08:45:52 -0600

    Create Jenkins.sh

---
+++ Jenkins.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+pip install -r requerimientos.txt
+autopep8 -ir *
+flake8 --max-complexity=5 --exclude=*.txt,.gitignore *
+lettuce Calculadora
+cd calculadoraUnitTest
+python TestCalcultor.py
+coverage run --branch TestCalcultor.py 
+coverage report -m 
+coverage html -title="cobertura del codigo mayor" 
